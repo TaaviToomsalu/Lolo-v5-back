266c0a9740ff8a7dfaeef20dd59769191472a1e7
Taavi Toomsalu <queenshelob@mail.com>
2024-05-07 09:48:35 +0300

    get and post request

--- index.js
+++ index.js
@@ -1,8 +1,78 @@
 const express = require('express')
+const axios = require('axios')
+const bodyParser = require('body-parser')
 
 const app = express()
-
 const port = 5001
+
+app.use(bodyParser.json())
+
+//Endpoint to fetch articles from the provided RSS feed
+app.get('/articles', async (req, res) => {
+    try {
+        const rssFeedUrl = 'https://flipboard.com/@raimoseero/feed-nii8kd0sz.rss'
+        const response = await axios.get(rssFeedUrl)
+
+        // Parse the XML response
+        const xmlString = response.data
+        const parser = require('xml2js').parseString
+        parser(xmlString, async (err, result) => {
+            if (err) {
+                console.error('Error parsing XML:', err)
+                throw new Error('Error parsing XML')
+            }
+
+            // Extract article URLs from the parsed XML
+            const articleUrls = result.rss.channel[0].item.map(item => item.link[0])
+
+            // Fetch and parse article content using the Mercury API
+            const clutterFreeArticles = await Promise.all(articleUrls.map(url => parseArticleContent(url)))
+
+            // Send clutter-free articles in the response
+            res.json(clutterFreeArticles)
+        })
+    } catch (error) {
+        console.error('Error fetching articles:', error)
+        res.status(500).json({ error: 'Internal Server Error' })
+    }
+
+})
+
+// Endpoint to add custom RSS feeds
+app.post('/feeds', (req, res) => {
+    try {
+      const { url } = req.body;
+  
+      // Perform validation checks on the URL if needed
+  
+      // Add the URL to a list of feeds (you can store it in memory or use a database)
+      // For demonstration purposes, we'll just log the URL
+      console.log('Custom RSS feed added:', url);
+  
+      res.json({ message: 'Custom RSS feed added successfully' });
+    } catch (error) {
+      console.error('Error adding custom RSS feed:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+// Function to fetch and parse article content using the Mercury API
+async function parseArticleContent(url) {
+    try {
+      const mercuryApiUrl = 'https://uptime-mercury-api.azurewebsites.net/webparser';
+      
+      // Make a POST request to the Mercury API with the article URL
+      const response = await axios.post(mercuryApiUrl, { url });
+
+      console.log('Mercury API response:', response);
+      
+      // Return the parsed article content
+      return response.data;
+    } catch (error) {
+      console.error('Error parsing article content:', error);
+      throw new Error('Error parsing article content');
+    }
+}
 
 app.get('/', (req, res) => {
     res.send('Hello, Lolo v5 Backend!')
