729c589bfad78fe0c325d6a229df6c0625ab2d2b
Taavi Toomsalu <queenshelob@mail.com>
2024-05-19 16:25:00 +0300

    cleaning up

--- index.js
+++ index.js
@@ -44,12 +44,12 @@ app.get('/articles', async (req, res) => {
                 return {
                     title: item.title[0],
                     link: item.link[0],
-                    pubDate: new Date(item.pubDate[0]), // Convert to Date object
+                    pubDate: new Date(item.pubDate[0]),
                     description: item.description[0],
                     author: author,
                     mediaContent: item['media:content'] ? item['media:content'][0].$ : null,
                     categories: categories,
-                    feedUrl: feedUrl // Add feed URL to each article
+                    feedUrl: feedUrl
                 };
             });
         });
